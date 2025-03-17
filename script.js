883925606195617cc01f4233560de4eb48fde4d1
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2025-03-17 18:04:15 +0100

    fixat så att man ser när man lägger till produkter. har samt gjort så att man kan ta bort dem

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 //Hämta från localstorage eller skapa en tom array
 let varukorg = JSON.parse(localStorage.getItem("varukorg")) || [];
 
-//Hitta varukorgens ul-element
-const ul = document.querySelector(ul);
-
 //Funktion för att lägga till i varukorgen och spara i local storage
-function LäggTill(namn, pris)
-{
-    let produkt = { Namn: namn, Pris: pris};
+function LäggTill(namn, pris) {
+    let produkt = { Namn: namn, Pris: pris };
     varukorg.push(produkt);
 
     localStorage.setItem("varukorg", JSON.stringify(varukorg));
@@ -15,7 +11,31 @@ function LäggTill(namn, pris)
 }
 
 //Funktion för att visa produkterna i varukorgen
-function VisaIVarukorg()
-{
-    
+function VisaIVarukorg() {
+    //Hitta varukorgens ul-element
+    let ul = document.querySelector("ul");
+
+    ul.innerHTML = "";
+
+    for (let index = 0; index < varukorg.length; index++) {
+        let vara = varukorg[index];
+        let li = document.createElement("li");
+        li.textContent = vara.Namn + " | " + vara.Pris + " SEK "
+
+        //Skapa en ta bort knapp
+        let tabortknapp = document.createElement("button");
+        tabortknapp.textContent = "Ta bort"
+        tabortknapp.onclick = function() {
+            TaBortFrånVagn(index);
+        };
+
+        li.appendChild(tabortknapp);
+        ul.appendChild(li);
+    }
+}
+
+function TaBortFrånVagn(index){
+    varukorg.splice(index, 1);
+    localStorage.setItem("varukorg", JSON.stringify(varukorg));
+    VisaIVarukorg(); //Uppdatera varukorgen
 }
